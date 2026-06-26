477a0d1119f8d47dd3bad452e77fc64cc790f2d4
rishi mourya <clipmenders@gmail.com>
2026-06-26 16:00:20 +0530

    update: homepage UI

---
+++ passop/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './component/Navbar'
+import Manager from './component/Manager'
+
+function App() {
+
+  return (
+    <>
+
+    <Navbar/>
+    
+   <Manager/>
+   </>
+  )
+}
+
+export default App
---
+++ passop/src/component/Manager.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Manager = () => {
+    return (
+        <>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+
+            <div className="  bg-slate-50 mx-auto px-40 py-16  mycontainer">
+
+                <h1 className=' text-4xl text font-bold text-center'>
+                    <span className='text-green-700'>&lt;</span>
+                    <span>Pass</span><span className='text-green-700'>OP/&gt;</span>
+
+                </h1>
+                <p className=' text-green-700 text-lg text-center'> Your Own Passward Manager </p>
+
+
+                <div className=" flex flex-col p-4 text-black gap-8">
+                    <input className=' rounded-full border border-green-700  w-full p-4 py-1 ' type='text' name='' id='' />
+                    <div className="flex w-full justify-between gap-8">
+
+                        <input className=' rounded-full border border-green-700  w-full p-4 py-1 ' type='text' name='' id='' />
+                        <input className=' rounded-full border border-green-700  w-full p-4 py-1 ' type='text' name='' id='' />
+                    </div>
+                    <button> Add Passward</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Manager
